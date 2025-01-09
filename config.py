4f5d78f87d2286dd7366c5f8dd631d4291052e21
sᴀɢᴀʀ ᴛɪᴡᴀʀɪ <112709751+stkeditz@users.noreply.github.com>
2025-01-09 20:54:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/arman786mariyam/SpyMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Spy")
 GIT_TOKEN = getenv(
