b0d82a50b29bb9d70f5efa326eabaf6eea80634f
arman786mariyam <mobashirahmad507@gmail.com>
2025-01-14 09:56:07 +0530

    Update thumbnails.py

--- Spy/utils/thumbnails.py
+++ Spy/utils/thumbnails.py
@@ -115,7 +115,7 @@ async def get_thumb(videoid):
         name_font = ImageFont.truetype("Spy/assets/font.ttf", 30)
         para = textwrap.wrap(title, width=30)
         j = 0
-        draw.text((5, 5), f"SpyXDil", fill="white", font=name_font)
+        draw.text((5, 5), f"SanyaXArman", fill="white", font=name_font)
         for line in para:
             if j == 1:
                 j += 1
